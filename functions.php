6ffb6993cfec8f58f17bf09b3325b6cfc7b5068e
katrinapulawska <katrinapulawska@gmail.com>
2019-04-23 23:16:25 +0200

    navbar

--- functions.php
+++ functions.php
@@ -2,9 +2,9 @@
 
 function enqueue_styles() {
 
-    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' );
     wp_enqueue_style( 'core', get_template_directory_uri() . '/style.css' );
-
+    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' );
+    wp_enqueue_style('customstyle', get_template_directory_uri() . '/style.css', array(), rand(111,9999), 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_styles');
 function themebs4_enqueue_scripts() {
--- header.php
+++ header.php
@@ -18,38 +18,44 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+
+
+
+
+ <div class="container-fluid">
+<nav class="navbar navbar-expand-lg navbar-light">
+  <a class="navbar-brand" href="#">
+	<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/survicate_logo.svg" width="185" height="35">
+	</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
+  <div class="collapse navbar-collapse vbottom" id="navbarNavDropdown">
+    <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">SURVEY SUITE</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
+        <a class="nav-link" href="#">FEEDBACK HUB</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+        <a class="nav-link" href="#">SOLUTIONS</a>
       </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">PRICING</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">CUSTOMERS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">RESOURCES</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link textbold" href="#">LOG IN</a>
+      </li>
+	<li  class="signup1"><a href="">sign up free</a></li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
 </nav>
+	</div>
